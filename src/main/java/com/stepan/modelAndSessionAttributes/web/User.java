fc6e1c5fef1901aee3f8c52cda03abefd3ac58b7
Stepan Razdobarov <northernpeople@gmail.com>
2017-11-07 16:20:23 -0600

    added sessionattribute

--- src/main/java/com/stepan/modelAndSessionAttributes/web/User.java
+++ src/main/java/com/stepan/modelAndSessionAttributes/web/User.java
@@ -5,6 +5,15 @@ public class User{
 	
 	private String name;
 
+	public User() {
+	}
+	
+
+	public User(String name) {
+		this.name = name;
+	}
+
+
 	public String getName() {
 		return name;
 	}
--- src/main/java/com/stepan/modelAndSessionAttributes/web/UserController.java
+++ src/main/java/com/stepan/modelAndSessionAttributes/web/UserController.java
@@ -1,31 +1,41 @@
 package com.stepan.modelAndSessionAttributes.web;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 @RequestMapping("/user")
+@SessionAttributes({"candidate"}) // adds candidate to session, as soon as getForm is called
 public class UserController {
-	
-	
-	@ModelAttribute("user")       // adding user before every method in this controller
-	public void addUser(Model m){
-		m.addAttribute("user", new User());
-	}
 	
 	@GetMapping
 	public String getForm(Model m){
-//		m.addAttribute("user", new User());
+		User u = new User();
+		m.addAttribute("user", u); // not in session
+		m.addAttribute("candidate", new User("candidate 1")); // in session
 		return "user_form";
 	}
 	
+	
 	@PostMapping
-	public String saveForm(@ModelAttribute User user, Model m){
-		System.out.println("got user: "+ user);
+	public String saveForm(@ModelAttribute User user, @ModelAttribute User candidate, Model m, SessionStatus status, HttpSession session){
+//		status.setComplete(); // removes attributes from session	
+//		Enumeration<String> e = session.getAttributeNames();
+//		while (e.hasMoreElements()){ System.out.println( e.nextElement()); }
+		 
+		System.out.println("candidate from session: "+ session.getAttribute("candidate"));
+		System.out.println("user from session: "+ session.getAttribute("user"));  
+		System.out.println("user from form: "+ user);
 		return "redirect:/user";
 	}
 
